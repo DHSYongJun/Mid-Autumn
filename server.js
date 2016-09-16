9549613685b4f18f4db85b4406dd7a09fe1993ec
DHSYongJun <lim.yongjun@dhs.sg>
2016-09-16 08:46:16 +0800

    fix server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
-'Content-Type': 'text/plain;charset=utf-8'
 
 app.listen(3000, function () {
 	  console.log('Example app listening on port 3000!');
